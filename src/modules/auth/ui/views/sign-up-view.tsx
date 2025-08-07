919ed3e36514f9915925c61a88eed80d2ade2edb
SAIRANTRI <pathaksairantri@gmail.com>
2025-08-07 15:03:42 +0530

    added a dashboard sidebar and changed themes

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -229,7 +229,7 @@ export const SignUpView = () => {
             </form>
           </Form>
 
-          <div className="bg-radial from-blue-700 to-blue-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center p-6">
+          <div className="bg-radial from-sidebar-accent to-sidebarrelative hidden md:flex flex-col gap-y-4 items-center justify-center p-6">
             <img src="/logo.svg" alt="Logo" className="h-[72px] w-[72px]" />
             <p className="text-2xl font-semibold text-white">Converse.AI</p>
           </div>
